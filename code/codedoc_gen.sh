7c0f7a5ed8540cf9cb43f628a03db1c82e626dee
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 21:48:51 -0400

    Making including easier.

--- code/codedoc_gen.sh
+++ code/codedoc_gen.sh
@@ -3,10 +3,10 @@
 CODE_LOC="$HOME/Democritus"
 LLVM_FILES="democritus.ml scanner.mll parser.mly semant.ml ast.ml codegen.ml"
 C_FILES="bindings.c"
+TEX_FILE=code.tex
 
 # iterate over each file and put typeseted copy into .tex file
 for filename in $LLVM_FILES; do
-    TEX_FILE=$filename.tex
     echo "\\section{$filename}" >> $TEX_FILE
     echo "\\begin{minted}[breaklines,linenos]{ocaml}" >> $TEX_FILE
     cat $CODE_LOC/$filename >> $TEX_FILE
@@ -14,7 +14,6 @@ for filename in $LLVM_FILES; do
 done
 
 for filename in $C_FILES; do
-    TEX_FILE=$filename.tex
     echo "\\section{$filename}" >> $TEX_FILE
     echo "\\begin{minted}[breaklines,linenos]{c}" >> $TEX_FILE
     cat $CODE_LOC/$filename >> $TEX_FILE
