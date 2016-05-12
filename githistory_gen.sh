a54c975c2042e4df848aa19e60ea354d3c5ed6a2
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 22:31:08 -0400

    Removed git --oneline. Need full commit info.

--- githistory_gen.sh
+++ githistory_gen.sh
@@ -7,7 +7,7 @@ touch $TEX_FILE
 echo "\\chapter{Git Logs}" >> $TEX_FILE
 echo "\\begin{lstlisting}" >> $TEX_FILE
 
-git --git-dir=$CODE_LOC/.git log --pretty=format:"%h %s" --graph >> $TEX_FILE 
+git --git-dir=$CODE_LOC/.git log --graph >> $TEX_FILE 
 
 echo -e "\n" >> $TEX_FILE
 echo "\\end{lstlisting}" >> $TEX_FILE
