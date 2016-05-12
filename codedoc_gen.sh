a8f3fa89688bcca7498d9d04d37197ded652c4a1
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 22:07:03 -0400

    Moved and edited script, and regenerated code.tex. code.tex not displaying.

--- codedoc_gen.sh
+++ codedoc_gen.sh
@@ -3,20 +3,26 @@
 CODE_LOC="$HOME/Democritus"
 LLVM_FILES="democritus.ml scanner.mll parser.mly semant.ml ast.ml codegen.ml"
 C_FILES="bindings.c"
+TEX_FILE="tex/code.tex"
+
+rm $TEX_FILE
+touch $TEX_FILE
+
+echo "\\chapter{Code Listing}" >> $TEX_FILE
 
 # iterate over each file and put typeseted copy into .tex file
 for filename in $LLVM_FILES; do
-    TEX_FILE=$filename.tex
     echo "\\section{$filename}" >> $TEX_FILE
-    echo "\\begin{minted}[breaklines,linenos]{ocaml}" >> $TEX_FILE
+    echo "\\begin{lstlisting}{ocaml}" >> $TEX_FILE
     cat $CODE_LOC/$filename >> $TEX_FILE
-    echo "\\end{minted}" >> $TEX_FILE 
+    echo "\\end{lstlisting}" >> $TEX_FILE
 done
 
 for filename in $C_FILES; do
-    TEX_FILE=$filename.tex
     echo "\\section{$filename}" >> $TEX_FILE
-    echo "\\begin{minted}[breaklines,linenos]{c}" >> $TEX_FILE
+    echo "\\begin{lstlisting}{c}" >> $TEX_FILE
     cat $CODE_LOC/$filename >> $TEX_FILE
-    echo "\\end{minted}" >> $TEX_FILE 
+    echo "\\end{lstlisting}" >> $TEX_FILE
 done
+
+sed -i 's/_/\\_/g' $TEX_FILE
