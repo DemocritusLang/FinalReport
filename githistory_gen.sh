9e711d3f249f321261fdcd1a4d58cea1547c3cd7
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 22:42:48 -0400

    Updated git history generator and added graph image.

--- githistory_gen.sh
+++ githistory_gen.sh
@@ -4,6 +4,9 @@ TEX_FILE=tex/git.tex
 rm $TEX_FILE
 touch $TEX_FILE
 
+echo "\\medskip \\noindent" >> $TEX_FILE
+echo "Note: our git log is greatly shortened because with few exceptions, we squashed and rebased our commits for a better understanding of our progression." >> $TEX_FILE
+
 echo "\\chapter{Git Logs}" >> $TEX_FILE
 echo "\\begin{lstlisting}" >> $TEX_FILE
 
